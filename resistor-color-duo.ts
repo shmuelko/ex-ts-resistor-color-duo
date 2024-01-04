b0740b809ae3ff22b4c13ab9ab9a97c00a1a5fd8
shmuelko <shmuel777@gmail.com>
2024-01-04 17:54:45 +0200

    complete

--- resistor-color-duo.ts
+++ resistor-color-duo.ts
@@ -1,3 +1,8 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(selectedColors: string[]): number {
+  const colors: string[] = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+
+  return Number(`${colors.indexOf(selectedColors[0])}${colors.indexOf(selectedColors[1])}`)
 }
+
+//   return 2;
+//   integer(colors.indexOf(selected-colors[0])) + integer(colors.indexOf(selected-colors[1]))
